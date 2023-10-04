02da9fa5dc5b5fa98a2171c689a3c8a1e152e77e
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-10-04 00:39:50 -0700

    textme second trial

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,21 +1,2 @@
 #!/usr/bin/env ruby
-
-# Extract information from the input string using regex
-matches = ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
-
-# Initialize arrays to store extracted data
-from_values = []
-to_values = []
-flags_values = []
-
-# Loop through the matches and extract values
-matches.each do |match|
-  from_values << match[0]
-  to_values << match[1]
-  flags_values << match[2]
-end
-
-# Join the extracted values with commas and print
-puts "From: #{from_values.join(", ")}"
-puts "To: #{to_values.join(", ")}"
-puts "Flags: #{flags_values.join(", ")}"
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
