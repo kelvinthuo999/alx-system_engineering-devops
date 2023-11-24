cb95a488fee22bbaba0713c065bf164096583bc4
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-11-24 06:35:57 -0800

    remove: class

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
 #!/usr/bin/pup
 # Manifest: Install Flask using pip3
 
-class { 'python':
-  version => 'system',
-}
-package { 'python3-pip':
-  ensure => 'installed',
-}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
