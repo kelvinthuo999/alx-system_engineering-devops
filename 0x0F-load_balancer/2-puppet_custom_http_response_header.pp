4adb11d7934132308e03d3185e44b914bd82a667
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-12-04 07:12:17 -0800

    fix: using exec command

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,9 @@
 # Puppet manifest to install and configure Nginx with a custom HTTP header
 
-package { 'nginx':
-  ensure  => 'installed',
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "server {\n    listen 80 default_server;\n    add_header X-Served-By ${::hostname};\n}\n",
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
