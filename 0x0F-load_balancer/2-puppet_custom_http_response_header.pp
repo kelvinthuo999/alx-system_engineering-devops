34f8bd6882c9f4dbb7733fa7eb0765afe02a0f93
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-12-04 07:04:03 -0800

    trial: another puppet file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,54 +1,9 @@
 # Puppet manifest to install and configure Nginx with a custom HTTP header
 
-# Ensure apt is up-to-date
-package { 'apt-update':
-  ensure => latest,
-}
-
-# Install Nginx
-package { 'nginx':
-  ensure => installed,
-  require => Package['apt-update'],
-}
-
-# Custom HTTP header value
-$custom_header_value = $facts['hostname']
-
-# Define Nginx site configuration with custom HTTP header
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
-    add_header X-Served-By $custom_header_value;
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-    location /redirect_me {
-        rewrite ^ https://youtube.com/watch?v=QH2-TGUlwu4 permanent;
-    }
-    error_page 404 /404.html;
-    location = /404.html {
-        internal;
-    }
-}",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Nginx service configuration
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-}
-
-# Notify systemd to reload Nginx when the site configuration changes
-exec { 'reload_nginx':
-  command     => 'systemctl reload nginx',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
