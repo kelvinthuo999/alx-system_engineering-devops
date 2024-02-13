7d277fc5e3c658f3edfd5f1dfc09ed045d3a5a2e
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-02-13 11:03:02 -0800

    create: python script to fetch api info

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""
-Module to query the Reddit API and return the number of subscribers for a given subreddit.
-"""
+"""Module to query the Reddit API"""
 
 import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Function to get the number of subscribers for a subreddit.
+    """Function to get the number of subscribers for a subreddit.
 
     Args:
-    - subreddit (str): The subreddit name.
+        subreddit (str): The subreddit name.
 
     Returns:
-    - int: The number of subscribers.
+        int: The number of subscribers, or 0 if the subreddit is invalid.
     """
+    headers = {'User-Agent': 'MyUniqueUserAgent'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    else:
-        return 0
+        return response.json().get("data").get("subscribers")
+    return 0
