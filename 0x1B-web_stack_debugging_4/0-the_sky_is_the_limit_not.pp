57cd0e357dda52f5ea4ee2327a0b6a89fd66a64b
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-02-20 00:19:49 -0800

    fix: puppet-lint requirements

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,13 @@
-# Eliminating request failures.
+# Adjusting ULIMIT for Nginx to handle more traffic.
 
-# Increase the ULIMIT
+# Fix ULIMIT for Nginx
 exec { 'fix-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
-} ->
+}
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => 'sudo service nginx restart',
-  path    => '/etc/init.d/',
-  require => Exec['fix-nginx'],
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
